3d3798730865580b3793b83c8a3eb77f29945af8
Nidin Vinayakan <nidinthb@gmail.com>
2014-07-23 18:29:48 +0200

    Read me updated

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ TS-7zip
 =======
 
 7zip Archive decoder written in TypeScript / JavaScript
+
+സെവൻ സിപ്പ്
+----------
+(Dedicated to my mother tongue :D , http://en.wikipedia.org/wiki/Malayalam)
+7zip Archive Decoder
+Version 0.1
+@author Nidin Vinayakan | nidinthb@gmail.com
+Released with MIT License (MIT)
--- src/nid/7zip/items/Folder.ts
+++ src/nid/7zip/items/Folder.ts
@@ -1,7 +1,7 @@
 module nid {
 
     /**
-     * സെവൻ സിപ്പ് (Dedicated to my mother tongue :D , http://en.wikipedia.org/wiki/Malayalam)
+     * സെവൻ സിപ്പ്  (Dedicated to my mother tongue :D , http://en.wikipedia.org/wiki/Malayalam)
      * 7zip Archive Decoder
      * Version 0.1
      * @author Nidin Vinayakan | nidinthb@gmail.com
